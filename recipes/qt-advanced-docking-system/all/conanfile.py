2c84cd132c45161825b19ef5aeaf39f55ce8bb8e
Frédéric Nadeau <fred.nadeau@gmail.com>
2023-10-10 21:51:46 -0400

    Update package

--- recipes/qt-advanced-docking-system/all/conanfile.py
+++ recipes/qt-advanced-docking-system/all/conanfile.py
@@ -1,6 +1,8 @@
 from conan import ConanFile
-from conan.tools.files import copy, get, apply_conandata_patches, export_conandata_patches, replace_in_file, rmdir
-from conans import CMake
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import collect_libs, copy, get, apply_conandata_patches, export_conandata_patches, replace_in_file, rmdir
+from conan.tools.scm import Version
+
 import os
 
 required_conan_version = ">=1.52.0"
@@ -26,17 +28,8 @@ class QtADS(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    generators = "cmake", "cmake_find_package", "cmake_find_package_multi"
-
-    _cmake = None
-    _qt_version = "5.15.6"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
 
     def export_sources(self):
-        copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
         export_conandata_patches(self)
 
     def config_options(self):
@@ -47,49 +40,59 @@ class QtADS(ConanFile):
         if self.options.shared:
             del self.options.fPIC
 
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
     def requirements(self):
-        self.requires(f"qt/{self._qt_version}")
+        self.requires("qt/[>=5.15 <7.0]")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.5 <4]")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True,
-                  destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_EXAMPLES"] = "OFF"
+        tc.variables["BUILD_STATIC"] = not self.options.shared
+        tc.generate()
 
-        self._cmake = CMake(self)
-        self._cmake.definitions["ADS_VERSION"] = self.version
-        self._cmake.definitions["BUILD_EXAMPLES"] = "OFF"
-        self._cmake.definitions["BUILD_STATIC"] = not self.options.shared
-
-        self._cmake.configure()
-        return self._cmake
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def _patch_sources(self):
         apply_conandata_patches(self)
 
         replace_in_file(self,
-            f"{self.source_folder}/{self._source_subfolder}/src/ads_globals.cpp",
+            os.path.join(self.source_folder, "src", "ads_globals.cpp"),
             "#include <qpa/qplatformnativeinterface.h>",
-            f"#include <{self._qt_version}/QtGui/qpa/qplatformnativeinterface.h>"
+            f"#include <{self.dependencies['qt'].ref.version}/QtGui/qpa/qplatformnativeinterface.h>"
         )
 
     def build(self):
         self._patch_sources()
-        cmake = self._configure_cmake()
+        cmake =CMake(self)
+        # Build needs to know about ADS_VERSION before toolchain is loaded
+        cmake.configure(variables={"ADS_VERSION": self.version})
         cmake.build()
 
     def package(self):
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE.txt", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake =CMake(self)
         cmake.install()
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         rmdir(self, os.path.join(self.package_folder, "license"))
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        if self.options.shared:
-            self.cpp_info.libs = ["qtadvanceddocking"]
+        if Version(self.dependencies["qt"].ref.version) >= "6.0":
+            qtadvanceddocking = "qt6advanceddocking"
         else:
+            qtadvanceddocking = "qt5advanceddocking"
+
+        self.cpp_info.set_property("cmake_file_name", qtadvanceddocking)
+        self.cpp_info.includedirs.append(os.path.join("include", qtadvanceddocking))
+        self.cpp_info.libs = collect_libs(self)
+
+        if not self.options.shared:
             self.cpp_info.defines.append("ADS_STATIC")
-            self.cpp_info.libs = ["qtadvanceddocking_static"]
