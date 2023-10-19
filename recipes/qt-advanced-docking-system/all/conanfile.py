63ca76c31b0d710930c4e6f54c5798455d4423a3
Frédéric Nadeau <fred.nadeau@gmail.com>
2023-10-19 23:23:23 +0000

    Add Conan 2.0 compatibility
    Rewrite files to bring closer to template. Use same name as upstream
    project.

--- recipes/qt-advanced-docking-system/all/conanfile.py
+++ recipes/qt-advanced-docking-system/all/conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 from conan.tools.files import copy, get, apply_conandata_patches, export_conandata_patches, replace_in_file, rmdir
-from conans import CMake
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.52.0"
@@ -17,6 +18,7 @@ class QtADS(ConanFile):
         "in many popular integrated development environments (IDEs) such as "
         "Visual Studio."
     )
+    package_type = "library"
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
@@ -26,14 +28,14 @@ class QtADS(ConanFile):
         "shared": False,
         "fPIC": True,
     }
-    generators = "cmake", "cmake_find_package", "cmake_find_package_multi"
-
-    _cmake = None
-    _qt_version = "5.15.6"
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def _qt_major(self):
+        return Version(self.dependencies["qt"].ref.version).major
+
+    @property
+    def _qt_semver(self):
+        return Version(self.dependencies["qt"].ref.version)
 
     def export_sources(self):
         copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
@@ -45,51 +47,62 @@ class QtADS(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires(f"qt/{self._qt_version}")
+        self.requires("qt/[>=5.15.7 <6]", transitive_headers=True, transitive_libs=True)
+
+    def validate(self):
+        if not (self.dependencies["qt"].options.gui and self.dependencies["qt"].options.widgets):
+            raise ConanInvalidConfiguration(f"{self.ref} requires qt gui and widgets")
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True,
-                  destination=self._source_subfolder)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake
-
-        self._cmake = CMake(self)
-        self._cmake.definitions["ADS_VERSION"] = self.version
-        self._cmake.definitions["BUILD_EXAMPLES"] = "OFF"
-        self._cmake.definitions["BUILD_STATIC"] = not self.options.shared
-
-        self._cmake.configure()
-        return self._cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["ADS_VERSION"] = self.version
+        tc.variables["BUILD_EXAMPLES"] = "OFF"
+        tc.variables["BUILD_STATIC"] = not self.options.shared
+        tc.variables["QT_VERSION_MAJOR"] = f"{self._qt_major}"
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def _patch_sources(self):
         apply_conandata_patches(self)
 
         replace_in_file(self,
-            f"{self.source_folder}/{self._source_subfolder}/src/ads_globals.cpp",
+            os.path.join(self.source_folder, "src", "ads_globals.cpp"),
             "#include <qpa/qplatformnativeinterface.h>",
-            f"#include <{self._qt_version}/QtGui/qpa/qplatformnativeinterface.h>"
+            f"#include <{self._qt_semver}/QtGui/qpa/qplatformnativeinterface.h>"
         )
 
     def build(self):
         self._patch_sources()
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, pattern="gnu-lgpl-v2.1.md", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         rmdir(self, os.path.join(self.package_folder, "license"))
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "ads")
+        self.cpp_info.set_property("cmake_target_name", f"ads::qt{self._qt_major}advanceddocking")
+        self.cpp_info.includedirs.append(os.path.join("include", f"qt{self._qt_major}advanceddocking"))
+        self.cpp_info.requires = ["qt::qtCore", "qt::qtGui", "qt::qtWidgets"]
+
         if self.options.shared:
-            self.cpp_info.libs = ["qtadvanceddocking"]
+            self.cpp_info.libs = [f"qt{self._qt_major}advanceddocking"]
         else:
             self.cpp_info.defines.append("ADS_STATIC")
-            self.cpp_info.libs = ["qtadvanceddocking_static"]
+            self.cpp_info.libs = [f"qt{self._qt_major}advanceddocking_static"]
