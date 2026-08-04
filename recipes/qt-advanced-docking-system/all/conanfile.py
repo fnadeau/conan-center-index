d57704682b722670f8bca891e1c6e386a5baccd7
Nadeau, Frédéric [2] <Nadeau.Frederic2@hydroquebec.com>
2026-08-03 20:34:49 -0400

    qt-advanced-docking-system: Add version 4.5.0 and 5.0.0

--- recipes/qt-advanced-docking-system/all/conanfile.py
+++ recipes/qt-advanced-docking-system/all/conanfile.py
@@ -33,43 +33,42 @@ class QtADS(ConanFile):
     }
     implements = ["auto_shared_fpic"]
 
-    @property
-    def _qt_major(self):
-        return Version(self.dependencies["qt"].ref.version).major
-
-    @property
-    def _min_cppstd(self):
-        if self._qt_major >= 6:
-            return 17
-        else:
-            return 14
-
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
         self.requires("qt/[>=5.15 <7]", transitive_headers=True)
 
-    def validate(self):
-        check_min_cppstd(self, self._min_cppstd)
-
     def build_requirements(self):
         self.tool_requires("qt/<host_version>")
         self.tool_requires("cmake/[>=3.27 <5]") # to be able to use CMAKE_AUTOMOC_EXECUTABLE
 
+    def validate(self):
+        if Version(self.dependencies["qt"].ref.version) >= "6.0.0":
+            check_min_cppstd(self, 17) # Qt6 requires C++17 as a minimum
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.cache_variables["ADS_VERSION"] = self.version
         tc.cache_variables["BUILD_EXAMPLES"] = "OFF"
         tc.cache_variables["BUILD_STATIC"] = not self.options.shared
-        # https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System/blob/a16d17a8bf375127847ac8f40af1ebcdb841b13c/src/CMakeLists.txt#L12
-        # TODO: the upstream Qt recipe should expose this variable
-        qt_version = str(self.dependencies["qt"].ref.version)
-        qt_include_root = self.dependencies["qt"].cpp_info.includedirs[0]
-        tc.cache_variables[f"Qt{self._qt_major}Gui_PRIVATE_INCLUDE_DIRS"] = os.path.join(qt_include_root, "QtGui", qt_version, "QtGui")
+        # The upstream CMakeLists.txt only sets CMAKE_MODULE_PATH inside an
+        # if(NOT ADS_VERSION) block. Since we supply ADS_VERSION, that block is
+        # skipped and src/CMakeLists.txt cannot find Versioning.cmake.
+        tc.cache_variables["CMAKE_MODULE_PATH"] = os.path.join(self.source_folder, "cmake", "modules").replace("\\", "/")
+
+        qt_tools_rootdir = self.conf.get("user.qt:tools_directory", None)
+        if qt_tools_rootdir:
+            tc.cache_variables["CMAKE_AUTOMOC_EXECUTABLE"] = os.path.join(qt_tools_rootdir, "moc.exe" if self.settings_build.os == "Windows" else "moc")
+            tc.cache_variables["CMAKE_AUTORCC_EXECUTABLE"] = os.path.join(qt_tools_rootdir, "rcc.exe" if self.settings_build.os == "Windows" else "rcc")
+
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
@@ -88,15 +87,16 @@ class QtADS(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        if Version(self.version) >= 4:
-            name = f"qt{self._qt_major}advanceddocking"
-            self.cpp_info.includedirs.append(os.path.join("include", name))
-            lib_name = f"{name}d" if self.settings.build_type == "Debug" else name
-        else:
-            lib_name = "qtadvanceddocking"
+        # Detect Qt major version from installed include directory name
+        qt_major = 6 if Version(self.dependencies["qt"].ref.version) >= "6.0.0" else 5
 
-        self.cpp_info.set_property("cmake_file_name", lib_name)
-        self.cpp_info.set_property("cmake_target_name", f"ads::{lib_name}")
+        base_name = f"qtadvanceddocking-qt{qt_major}"
+        self.cpp_info.includedirs.append(os.path.join("include", base_name))
+        cmake_name = f"qt{qt_major}advanceddocking"
+        lib_name = f"{base_name}d" if self.settings.build_type == "Debug" else base_name
+
+        self.cpp_info.set_property("cmake_file_name", cmake_name)
+        self.cpp_info.set_property("cmake_target_name", f"ads::{cmake_name}")
 
         if self.options.shared:
             self.cpp_info.libs = [lib_name]
